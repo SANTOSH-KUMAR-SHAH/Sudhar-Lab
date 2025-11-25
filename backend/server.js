6ad83a5127457133344367fb9852a6e891624974
Param Khodiyar <paramkhodiyar@Params-MacBook-Pro.local>
2025-11-25 19:38:47 +0530

    Cors error

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const corsoptions = require('./utils/cors')
 const app = express();
 app.use(express.json());
 app.use(cors(corsoptions));
-app.options("*", cors(corsoptions));
 app.use(cookieParser());
 app.use('/api', require('./routes'));
 
