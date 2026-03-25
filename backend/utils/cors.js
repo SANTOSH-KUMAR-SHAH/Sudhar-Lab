b5dac192b4388d7fece3a19139546cd7bfda391c
Param Khodiyar <paramkhodiyar1008@gmail.com>
2026-03-25 15:37:36 +0530

    Updated cors

--- backend/utils/cors.js
+++ backend/utils/cors.js
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const corsOptions = {
   origin: isProd
-    ? "https://localhelpfrontendv2.vercel.app"
-    : "http://localhost:3000",
+    ? ["https://localhelpfrontendv2.vercel.app", "https://localhelp-app.vercel.app"]
+    : ["http://localhost:3000", "http://localhost:3001"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
